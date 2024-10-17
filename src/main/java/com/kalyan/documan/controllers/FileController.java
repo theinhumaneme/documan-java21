25b6ae1d1f66fb3945eca17f3b193b7611cb30cf
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2024-10-17 20:55:31 +0530

    return ResponseEntity
    use `Response Entity` to return payload as it is more extensible and flexible with status codes and headers

---
+++ src/main/java/com/kalyan/documan/controllers/FileController.java
@@ -0,0 +1,30 @@
+/* Copyright (C)2024 Mudumby Kalyan / @theinhumaneme  */
+package com.kalyan.documan.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileController {
+  private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+  @GetMapping("/api/v1/file")
+  public ResponseEntity<?> getFile(@RequestParam("fileId") Long fileId) {
+    return ResponseEntity.status(HttpStatus.OK).body("Here is your file");
+  }
+
+  @PostMapping("/api/v1/file")
+  public ResponseEntity<?> createFile(@RequestParam("file") MultipartFile file) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK).body(("File Uploaded Successfully"));
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An error occurred while processing the file.");
+    }
+  }
+}
--- src/main/java/com/kalyan/documan/controllers/TestController.java
+++ src/main/java/com/kalyan/documan/controllers/TestController.java
@@ -1,13 +1,15 @@
 /* Copyright (C)2024 Mudumby Kalyan / @theinhumaneme  */
 package com.kalyan.documan.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
   @GetMapping("/")
-  public String test() {
-    return "Hello World";
+  public ResponseEntity<?> test() {
+    return ResponseEntity.status(HttpStatus.OK).body("Hello World");
   }
 }
