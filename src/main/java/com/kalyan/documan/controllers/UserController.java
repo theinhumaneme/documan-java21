0d3c0583d8f154003a3b651ea58637cd94f003ec
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2024-10-25 21:40:37 +0530

    Update UserController.java

--- src/main/java/com/kalyan/documan/controllers/UserController.java
+++ src/main/java/com/kalyan/documan/controllers/UserController.java
@@ -6,8 +6,10 @@
 // sublicense, and/or sell copies of the software.
 package com.kalyan.documan.controllers;
 
+import com.kalyan.documan.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +17,35 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
   private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+  private final UserService userService;
 
-  @GetMapping("/api/v1/user")
-  public ResponseEntity<?> getUser(@RequestParam("userId") Long userId) {
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/api/v1/user/id")
+  public ResponseEntity<?> getUser(
+      @RequestParam(value = "userId", required = true) Integer userId) {
     try {
-      if (userId != null) {
-        return ResponseEntity.status(HttpStatus.OK).body("Here is your User");
-      } else {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Expected parameter userId");
-      }
+      return userService
+          .findById(userId)
+          .map(ResponseEntity::ok)
+          .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An error occurred while processing your request");
+    }
+  }
+
+  @GetMapping("/api/v1/user/username")
+  public ResponseEntity<?> getUser(@RequestParam("username") String username) {
+    try {
+      return userService
+          .findByUsername(username)
+          .map(ResponseEntity::ok)
+          .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     } catch (Exception e) {
       logger.error(e.getMessage());
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
