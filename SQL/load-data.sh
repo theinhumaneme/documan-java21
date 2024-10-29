b881f6ca33096e1cd68bd61194f81c01a5c01634
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2024-10-29 19:01:10 +0530

    add extension and subject init to load-data.sh

--- SQL/load-data.sh
+++ SQL/load-data.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 export PGPASSWORD='password'
+psql -h localhost -U root -d postgres -f enable-crypto-extension.sql
 psql -h localhost -U root -d postgres -f role.sql
 psql -h localhost -U root -d postgres -f year.sql
 psql -h localhost -U root -d postgres -f semester.sql
 psql -h localhost -U root -d postgres -f department.sql
+psql -h localhost -U root -d postgres -f subject.sql
 psql -h localhost -U root -d postgres -f user.sql
 psql -h localhost -U root -d postgres -f post.sql
 psql -h localhost -U root -d postgres -f comment.sql
