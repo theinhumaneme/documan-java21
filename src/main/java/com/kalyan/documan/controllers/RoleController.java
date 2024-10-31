8c4ee12c1a3fd8066cc8524fe34b0c1de4615ed7
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2024-11-01 00:26:43 +0530

    implement rolecontroller and role service

---
+++ src/main/java/com/kalyan/documan/controllers/RoleController.java
@@ -0,0 +1,98 @@
+// MIT License
+//
+// Copyright (C) 2024 Kalyan Mudumby / @theinhumaneme
+//
+// Permission is granted to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the software.
+package com.kalyan.documan.controllers;
+
+import com.kalyan.documan.service.RoleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/api/v1/role")
+public class RoleController {
+
+  private static final Logger log = LoggerFactory.getLogger(RoleController.class);
+  private final RoleService roleService;
+
+  public RoleController(RoleService roleService) {
+    this.roleService = roleService;
+  }
+
+  @GetMapping()
+  public ResponseEntity<?> getRole(@RequestParam("roleId") Integer roleId) {
+    try {
+      return roleService
+          .getRoleById(roleId)
+          .map(ResponseEntity::ok)
+          .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An error occurred while processing your request");
+    }
+  }
+
+  @GetMapping("/all")
+  public ResponseEntity<?> getAllRoles() {
+    try {
+      return roleService
+          .getAllRoles()
+          .map(ResponseEntity::ok)
+          .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An error occurred while processing your request");
+    }
+  }
+
+  @GetMapping("/user")
+  public ResponseEntity<?> getUserRole(@RequestParam("userId") Integer userId) {
+    try {
+      return roleService
+          .getUserRole(userId)
+          .map(ResponseEntity::ok)
+          .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An error occurred while processing your request");
+    }
+  }
+
+  @PutMapping("/promote")
+  public ResponseEntity<?> promoteUser(
+      @RequestParam("userId") Integer userId, @RequestParam("roleId") Integer roleId) {
+    try {
+      return roleService
+          .promoteUser(userId, roleId)
+          .map(ResponseEntity::ok)
+          .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An error occurred while processing your request");
+    }
+  }
+
+  @PutMapping("/demote")
+  public ResponseEntity<?> demoteUser(
+      @RequestParam("userId") Integer userId, @RequestParam("roleId") Integer roleId) {
+    try {
+      return roleService
+          .demoteUser(userId, roleId)
+          .map(ResponseEntity::ok)
+          .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An error occurred while processing your request");
+    }
+  }
+}
