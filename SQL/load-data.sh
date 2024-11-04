0e7db4429571b04aab96415ca2fa26881dc203ec
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2024-11-05 01:14:07 +0530

    update creds

--- SQL/load-data.sh
+++ SQL/load-data.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
 export PGPASSWORD='password'
-psql -h localhost -U root -d postgres -f enable-crypto-extension.sql
-psql -h localhost -U root -d postgres -f role.sql
-psql -h localhost -U root -d postgres -f year.sql
-psql -h localhost -U root -d postgres -f semester.sql
-psql -h localhost -U root -d postgres -f department.sql
-psql -h localhost -U root -d postgres -f subject.sql
-psql -h localhost -U root -d postgres -f user.sql
-psql -h localhost -U root -d postgres -f post.sql
-psql -h localhost -U root -d postgres -f comment.sql
+export host='localhost'
+export user='root'
+export port=5432
+psql -h $host -U $user -d postgres -p $port -f enable-crypto-extension.sql
+psql -h $host -U $user -d postgres -p $port -f role.sql
+psql -h $host -U $user -d postgres -p $port -f year.sql
+psql -h $host -U $user -d postgres -p $port -f semester.sql
+psql -h $host -U $user -d postgres -p $port -f department.sql
+psql -h $host -U $user -d postgres -p $port -f subject.sql
+psql -h $host -U $user -d postgres -p $port -f user.sql
+psql -h $host -U $user -d postgres -p $port -f post.sql
+psql -h $host -U $user -d postgres -p $port -f comment.sql
 
